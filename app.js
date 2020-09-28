351b4dea9f004e341dd28bf5f6ec708be473b9ba
Eugene Iurkevych <eugeneiurkevych@Eugenes-MacBook-Pro.local>
2020-09-28 19:14:28 +0200

    2020

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello YOu, Bro !!! you can do better!!!!! yes you can '));
+app.get('/', (req, res) => res.send("January is the first Mounth of the YEAR 2020! "));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
